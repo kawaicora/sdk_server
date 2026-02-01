69ae0059630b52090eae62c8fd4e4fa51f638337
kawaicora <mengyou81219@163.com>
2026-02-01 13:50:57 +0100

    更新路由

--- sdk_server/app/route/view_service.py
+++ sdk_server/app/route/view_service.py
@@ -30,15 +30,13 @@ import xml.etree.ElementTree as ET
 
     
 @bp.route("/")
-def view_index():
-    response = make_response(render_template("index.html"),200)
-    response.headers['X-Organization'] = 'Nintendo'
-    return response
 @bp.route("/view/")
 def view_index():
     response = make_response(render_template("index.html"),200)
     response.headers['X-Organization'] = 'Nintendo'
     return response
+
+
 # @bp.route("/favicon.ico")
 # def view_favicon():
 #     return send_from_directory("static/img","favicon.ico")
